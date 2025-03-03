73166c336ade7f2490134bfc7abad4fcebe14b97
Bastien <bastien-stachs@hotmail.fr>
2025-03-03 10:49:11 +0100

    ajout du schéma Article

---
+++ models/articles.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const articleSchema = mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "users"},
+    stripeProductId: { type: String },
+    articleCreationDate: { type: Date },
+    title: { type: String },
+    productDescription: { type: String },
+    category: { type: String },
+    itemType: { type: String },
+    condition: { type: String }, // "new", "pristine", "good"
+    price: { type: Number },
+    vat: { type: Number, default: 0 },
+    currency: { type: String, default: "EUR" },
+    pictures: [{ type: String }],
+    usersLikers: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+});
+
+const Article = mongoose.model('articles', articleSchema);
+
+module.exports = Article;
