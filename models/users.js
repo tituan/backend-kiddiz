16b1eaffced975f07b2afca8d650efaf72b08b42
Your name <Your email>
2025-03-03 11:30:04 +0100

    signUp road on loading

---
+++ models/users.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new Schema({
+  number: { type: Number },
+  line1: { type: String },
+  line2: { type: String }, // Optionnel
+  zipCode: { type: Number },
+  city: { type: String },
+  state: { type: String },
+  country: { type: String },
+});
+
+const termsSchema = new Schema({
+  conditionUtilisation: { type: Boolean },
+  publicy: { type: Boolean },
+});
+
+const userSchema = mongoose.Schema({
+  email: { type: String },
+  password: { type: String },
+  firstname: { type: String },
+  lastname: { type: String },
+  password: { type: String },
+  token: { type: String },
+  dateOfBirth: { type: Date },
+  registrationDate: { type: Date },
+  address: addressSchema, // Intégration du sous-schéma d'adresse
+  picture: { type: String },
+  phoneNumber: Number,
+  note: { type: String },
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  stripeCustomerId: { type: String },
+  socketId: { type: String },
+  terms: termsSchema,
+  status: {
+    type: String,
+    enum: ["active", "banned", "pending"],
+    default: "active",
+  },
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const User = require('../models/users.js');
+const { checkBody } = require('../modules/checkBody');
+const jwt = require('jsonwebtoken');
+const { expressjwt: expressJwt } = require('express-jwt');
+const bcrypt = require('bcrypt');
+router.post('/signup', async (req, res) => {
+  try{
+    if (!checkBody(req.body, ['username', 'password'])) {
+      res.json({ result: false, error: 'Missing or empty fields' });
+      return;
+    }
+  
+    // Check if the user has not already been registered
+    const existingUser = await User.findOne({ username: req.body.username })
+    if (existingUser === null && req.body.password === req.body.confirmPassword) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+      const {firstname,lastname, email, dateOfBirth } = req.body,
+        const newUser = new User({
+          firstname,
+          lastname,
+          email,
+          dateOfBirth, 
+          password: hash,
+          tokenUser: ,
+        });
+  
+        const savedUser = await newUser.save();
+        
+          res.json({ result: true, newUser });
+        
+      } else {
+        // User already exists in database
+        res.json({ result: false, error: 'User already exists' });
+      };}
+    catch (error) {
+    // Gérer les erreurs éventuelles
+    res.status(500).json({ result: false, message: 'An error has occurred.', error });
+}
+});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post ('/signin', async (req, res) => {
+  try{
+    if (!checkBody(req.body, ['username', 'password'])) {
+      res.json({ result: false, error: 'Missing or empty fields' });
+      return;
+    }
+    
+    const userData = await User.findOne({ username: req.body.username } && { admin : true})
+      if (userData && bcrypt.compareSync(req.body.password, data.password)) {
+        res.json({ result: true, token: data.token });
+      } else {
+        res.json({ result: false, error: 'User not found or wrong password' });
+      }
+    ;
+
+  }catch (error) {
+    // Handle any errors
+    res.status(500).json({ result: false, message: 'An error has occurred.', error: error.message });
+}
 });
 
 module.exports = router;
